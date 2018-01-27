024d4c1caa3abd33f4648e0a1052bb4e6aa1f826
Ellen Lee <leechinghui.tw@gmail.com>
2018-01-27 16:11:13 +0800

    generate uploader photo_image and update index and show template

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,4 @@
 class Photo < ApplicationRecord
-  validates_presence_of :title, :date, :description, :file_location  
+  mount_uploader :file_location, PhotoImageUploader
+  validates_presence_of :title, :date, :description, :file_location
 end
