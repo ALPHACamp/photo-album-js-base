2d4abaa602bbc34be5955e62900d402549c3163e
Ellen Lee <leechinghui.tw@gmail.com>
2018-01-27 13:16:51 +0800

    create photo model

---
+++ db/migrate/20180127051541_create_photos.rb
@@ -0,0 +1,11 @@
+class CreatePhotos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :photos do |t|
+      t.string :title
+      t.date :date
+      t.text :description
+      t.string :file_location
+      t.timestamps
+    end
+  end
+end
