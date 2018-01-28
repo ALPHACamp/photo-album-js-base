d069c20a30969c877118b50331591b109f7beec6
Ellen Lee <leechinghui.tw@gmail.com>
2018-01-28 21:24:18 +0800

    add index action for photo api

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::PhotosController < ApiController
+
+  # GET http://localhost:3000/api/v1/photos 
+  def index
+    @photos = Photo.all
+    render json: {
+      data: @photos.map do |photo|
+        {
+          title: photo.title,
+          date: photo.date,
+          description: photo.description
+        }
+      end
+    }
+  end
 end
