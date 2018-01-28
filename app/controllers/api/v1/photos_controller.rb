9f887cf0de613d2e7af21a8a08bfa494ea5b6057
Ellen Lee <leechinghui.tw@gmail.com>
2018-01-28 21:27:11 +0800

    add create action for photo api

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -31,4 +31,25 @@ class Api::V1::PhotosController < ApiController
     end
   end
 
+  # POST http://localhost:3000/api/v1/photos
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      render json: {
+        message: "Photo created successfully!",
+        result: @photo
+      }
+    else
+      render json: {
+        errors: @photo.errors
+      }
+    end
+  end
+
+  private
+
+  def photo_params
+    params.permit(:title, :date, :description, :file_location)
+  end
+
 end
