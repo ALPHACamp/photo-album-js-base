401c7f028061f35733faeb578230079ef679ef74
Ellen Lee <leechinghui.tw@gmail.com>
2018-01-28 23:01:36 +0800

    add generate_authentication_token method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  before_create :generate_authentication_token
+
+  def generate_authentication_token
+    # Devise.friendly_token 會自動產生 20 字元長的亂數
+    self.authentication_token = Devise.friendly_token
+  end
 end
