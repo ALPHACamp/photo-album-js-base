89a312689debe6a53cb47894894195cc1b30f253
Ellen Lee <leechinghui.tw@gmail.com>
2018-01-28 21:24:53 +0800

    add show action for photo api

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PhotosController < ApiController
 
-  # GET http://localhost:3000/api/v1/photos 
+  # GET http://localhost:3000/api/v1/photos
   def index
     @photos = Photo.all
     render json: {
@@ -13,4 +13,22 @@ class Api::V1::PhotosController < ApiController
       end
     }
   end
+
+  # GET http://localhost:3000/api/v1/photos/:id
+  def show
+    @photo = Photo.find_by(id: params[:id])
+    if !@photo
+      render json: {
+        message: "Can't find the photo!",
+        status: 400
+      }
+    else
+      render json: {
+        title: @photo.title,
+        date: @photo.date,
+        description: @photo.description
+      }
+    end
+  end
+
 end
